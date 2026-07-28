a8602d81c729b39fe74b5bc3121d943437c2e3f4
BurningHot687 <222676217+BurningHot687@users.noreply.github.com>
2026-07-27 22:17:55 -0500

    test(zlib-ver): implement copy-pasted test for `zlib-ver` example

--- csalt/tests/build_tests.rs
+++ csalt/tests/build_tests.rs
@@ -4,6 +4,8 @@ use tempfile::tempdir;
 
 use csalt::{build_managed_project, build_manual_project};
 
+// TODO: If you're going to use the `examples/` as the source for many tests, why not just loop over 'em? Unable to see exactly what went wrong without it clogging the whole thing perhaps? Figure it out, copy-pasting sucks
+
 // TODO: Move to use nice function in fs_utils, `walk_and_filter_dirs()` later
 fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> anyhow::Result<()> {
     fs::create_dir_all(&dst)?;
@@ -67,6 +69,69 @@ fn test_default_project_self_build_system() -> anyhow::Result<()> {
     let example_src = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("examples")
         .join("default-new-project");
+    if example_src.exists() {
+        copy_dir_all(&example_src, test_root)?;
+    } else {
+        anyhow::bail!("example source directory does not exist");
+    }
+
+    build_manual_project(
+        &None,
+        &Some(PathBuf::from(test_root)),
+        &None,
+        false,
+        &None,
+        true,
+        &Vec::new(),
+    )?;
+
+    assert!(cache_dir.exists(), "cache directory was not created!");
+
+    Ok(())
+}
+
+#[test]
+fn test_zlib_ver_example_project_cmake_generation() -> anyhow::Result<()> {
+    let temp_dir = tempdir()?;
+    let test_root = temp_dir.path();
+    let cache_dir = test_root.join(".csalt");
+
+    let example_src = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("examples")
+        .join("zlib-ver");
+    if example_src.exists() {
+        copy_dir_all(&example_src, test_root)?;
+    } else {
+        anyhow::bail!("example source directory does not exist");
+    }
+
+    build_managed_project(
+        &None,
+        &Some(PathBuf::from(test_root)),
+        &None,
+        &Vec::new(),
+        &None,
+        true,
+    )?;
+
+    let expected_cmake_path = cache_dir.join("CMakeLists.txt");
+    assert!(
+        expected_cmake_path.exists(),
+        "CMakeLists.txt was not generated inside the cache directory!"
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_zlib_ver_self_build_system() -> anyhow::Result<()> {
+    let temp_dir = tempdir()?;
+    let test_root = temp_dir.path();
+    let cache_dir = test_root.join(".csalt");
+
+    let example_src = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("examples")
+        .join("zlib-ver");
     if example_src.exists() {
         copy_dir_all(&example_src, test_root)?;
     } else {
